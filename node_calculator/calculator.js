cf11b9689fc3860a6ef2948aacbe72bf9bdaf241
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-02-05 00:43:27 -0500

    Initial Commit for completed assignment

---
+++ node_calculator/calculator.js
@@ -0,0 +1,35 @@
+//---Addition Function
+function letsAdd (val1, val2) {
+    let sum = val1 + val2;
+    return `The sum is ${sum}`;
+};
+
+// code test for letsAdd invoke
+console.log(letsAdd(4, 5))
+
+//---Multiply Function
+function letsMult (val1, val2) {
+    let product = val1 * val2;
+    return `The product is ${product}`;
+};
+
+//code test for letsMult invoke
+console.log(letsMult(4, 5))
+
+//---Division function
+function letsDiv(val1, val2) {
+    let quotient = val1 / val2;
+    return `The quotient is ${quotient}`;
+};
+
+//code test for letsDiv invoke
+console.log(letsDiv(4,5))
+
+//---Subtract Function
+function letsSubtract(val1, val2) {
+    let difference = val1 - val2;
+    return `The difference is ${difference}`;
+};
+
+//code test for letsSubtract
+console.log(letsSubtract(4, 5))

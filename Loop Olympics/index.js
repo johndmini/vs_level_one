0549b5ec012a9a8d75d95fc9e7940c0fa02935fa
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-02-12 22:45:49 -0500

    Loop Olympics Final Commit

--- Loop Olympics/index.js
+++ Loop Olympics/index.js
@@ -18,7 +18,7 @@
 // }
 
 //---BRONZE MEDAL --------------------------------
-//---Numbers Array
+// // ---Numbers Array
 // let numbersArray = [];
 // for(let i = 0; i <= 9; i++){
 //     numbersArray.push(i);
@@ -42,24 +42,24 @@
 // console.log(fruitArray);
 
 //---SILVER MEDAL --------------------------------
-const peopleArray = [
-  {
-    name: "Harrison Ford",
-    occupation: "Actor"
-  },
-  {
-    name: "Justin Bieber",
-    occupation: "Singer"
-  },
-  {
-    name: "Vladimir Putin",
-    occupation: "Politician"
-  },
-  {
-    name: "Oprah",
-    occupation: "Entertainer"
-  }
-]
+// const peopleArray = [
+//   {
+//     name: "Harrison Ford",
+//     occupation: "Actor"
+//   },
+//   {
+//     name: "Justin Bieber",
+//     occupation: "Singer"
+//   },
+//   {
+//     name: "Vladimir Putin",
+//     occupation: "Politician"
+//   },
+//   {
+//     name: "Oprah",
+//     occupation: "Entertainer"
+//   }
+// ]
 
 // let namesArray = [];
 // let occupationsArray = [];
@@ -72,10 +72,53 @@ const peopleArray = [
 // console.log(namesArray)
 // console.log(occupationsArray)
 
-let everOtherNames = [];
-let everOtherOccupation = [];
+// let everOtherNames = [];
+// let everOtherOccupation = [];
 
-for (let i = 0; i < peopleArray.length; i+2) {
-  everOtherNames.push(peopleArray[i].name)
+// for (let i = 0; i < peopleArray.length; i+=2) {
+//   everOtherNames.push(peopleArray[i].name)
+// }
+// console.log(everOtherNames);
+
+// for (let i = 1; i < peopleArray.length; i+=2) {
+//   everOtherOccupation.push(peopleArray[i].occupation)
+// }
+// console.log(everOtherOccupation);
+
+//---GOLD MEDAL -----------------------------
+let numbersArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let arrayOne = [];
+
+for (let i = 0; i < numbersArray.length; i += 3) {
+  arrayOne.push(numbersArray.slice(i, i+3));
 }
-console.log(everOtherNames);
+console.log(arrayOne);
+
+let numbersArrayTwo = [0, 0, 0, 1, 1, 1, 2, 2, 2];
+let arrayTwo = [];
+
+for (let i = 0; i < numbersArrayTwo.length; i += 3) {
+  arrayTwo.push(numbersArrayTwo.slice(i, i+3));
+}
+console.log(arrayTwo);
+
+let numbersArrayThree = [0, 1, 2, 0, 1, 2, 0, 1, 2]
+let arrayThree = [];
+
+for (let i = 0; i < numbersArrayThree.length; i += 3) {
+  arrayThree.push(numbersArrayThree.slice(i, i+3));
+}
+console.log(arrayThree);
+
+const numbersForNow = [ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ];
+let lettersForLater = [];
+
+for(let i = 0; i < numbersForNow.length; i++){
+  for(let j = 0; j < numbersForNow[i].length; j++){
+    if(numbersForNow[i][j] === numbersForNow[i][j]){
+      numbersForNow[i][j] = lettersForLater.push('X')
+    }
+  }
+}
+
+console.log(lettersForLater)

b2e18569f628f2a4533b617fb4534770c3006aa2
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-02-18 02:34:47 -0500

    node calculator update

--- node_calculator/calculator.js
+++ node_calculator/calculator.js
@@ -1,38 +1,63 @@
 const rl = require('readline-sync');
         
-
 //---Addition Function
-function letsAdd (val1, val2) {
+letsAdd()
+function letsAdd () {
+    let val1 = Math.floor(Math.random() * 10 + 1);
+    let val2 = Math.floor(Math.random() * 10 + 1);
     let sum = val1 + val2;
-    return `The sum is ${sum}`;
+    let answer = rl.question(`What is the sum of ${val1} and ${val2}?\n`)
+    if(answer = sum){
+        console.log('You are correct')
+        letsMult();
+    } else {
+        console.log('Incorrect!! This is simple math!! Try again!!')
+        letsAdd();
+    }
 };
 
-// code test for letsAdd invoke
-// console.log(letsAdd(4, 5))
-
 //---Multiply Function
-// function letsMult (val1, val2) {
-//     let product = val1 * val2;
-//     return `The product is ${product}`;
-// };
+function letsMult () {
+    let val1 = Math.floor(Math.random() * 10 + 1);
+    let val2 = Math.floor(Math.random() * 10 + 1);
+    let product = val1 * val2;
+    let answer = rl.question(`What is the product of ${val1} and ${val2}?\n`)
+    if(answer = product){
+        console.log('You are correct')
+        letsDiv();
+    } else {
+        console.log('Incorrect!! This is simple math!! Try again!!')
+        letsMult();
+    }
+};
 
-// //code test for letsMult invoke
-// console.log(letsMult(4, 5))
+//---Division function
+function letsDiv() {
+    let val1 = Math.floor(Math.random() * 10 + 1);
+    let val2 = Math.floor(Math.random() * 10 + 1);
+    let quotient = val1 / val2;
+    let answer = rl.question(`What is the quotient of ${val1} and ${val2}?\n`)
+    if(answer = quotient){
+        console.log('You are correct')
+        letsSubtract();
+    } else {
+        console.log('Incorrect!! This is simple math!! Try again!!')
+        letsDiv();
+    }
+};
 
-// //---Division function
-// function letsDiv(val1, val2) {
-//     let quotient = val1 / val2;
-//     return `The quotient is ${quotient}`;
-// };
-
-// //code test for letsDiv invoke
-// console.log(letsDiv(4,5))
-
-// //---Subtract Function
-// function letsSubtract(val1, val2) {
-//     let difference = val1 - val2;
-//     return `The difference is ${difference}`;
-// };
-
-// //code test for letsSubtract
-// console.log(letsSubtract(4, 5))
+//---Subtract Function
+function letsSubtract() {
+    let val1 = Math.floor(Math.random() * 10 + 1);
+    let val2 = Math.floor(Math.random() * 10 + 1);
+    let difference = val1 - val2;
+    let answer = rl.question(`What is the difference between ${val1} and ${val2}?\n`)
+    if(answer = difference){
+        console.log('You are correct')
+        console.log('That is the end of the excercise!!')
+        process.exit(0);
+    } else {
+        console.log('Incorrect!! This is simple math !! Try again!!')
+        letsSubtract();
+    }
+};
